9fded343f6f7e666a11b13bbcea6019ab677c03f
mrw <lwilmoth>
2025-02-26 14:34:31 -0500

    made movie cards

---
+++ movie-site/src/components/MovieCard.jsx
@@ -0,0 +1,26 @@
+
+
+
+
+function MovieCard({ movie }){
+    function handleLike(){
+        alert(`${movie.title} was added to favorites`)
+    }
+    return(
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt="movie.title" />
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={handleLike}>♡</button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default MovieCard
