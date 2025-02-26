ae55480df487a8b9f58cb230367060cd14930862
mrw <lwilmoth>
2025-02-26 12:19:38 -0500

    added startup script

---
+++ startup.sh
@@ -0,0 +1,39 @@
+sudo apt update
+sudo apt upgrade
+
+sudo apt install curl
+
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.14.0".
+nvm current # Should print "v22.14.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
+
+
+
+
+
+
+
+
+# Start the vite project:
+npm create vite@latest
+
+
+# Install the dependencies:
+#cd my-vite-project # Change to the project directory.
+#npm install # or npm i
+
+#npm install react-router-dom
